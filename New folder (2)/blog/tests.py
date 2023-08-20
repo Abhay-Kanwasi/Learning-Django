cf48068ab53e7d0d3f56e19c2bf70330663553c9
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-20 22:22:23 +0530

    2 test case added

--- New folder (2)/blog/tests.py
+++ New folder (2)/blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+# Replicate into a new database
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    # It will be run first
+    def setUp(self):
+        # It will create some new data using default model manager(objects)
+        self.blog = Post.objects.create(title = 'django', author = 'django', slug = 'django')
+
+    def test_post_model(self):
+        d = self.blog
+        # data is d and database is Post If blog worked and data stored in database(Post) it will return True (Means OK)
+        self.assertTrue(isinstance(d, Post)) 
+        # We will check if two things are equal
+        self.assertEqual(str(d), 'django') # d will return django and in second parameter we giving 'django' to check if str(d) value is equal to django
+
