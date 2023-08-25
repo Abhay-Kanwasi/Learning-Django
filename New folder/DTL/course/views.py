919c0b201f069c6e48243a53cf59888a4a0020e9
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-08-25 22:16:07 +0530

    deleted

--- New folder/DTL/course/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def learn_django(request):
-    cname = 'Django'
-    duration = '4 Months'
-    seats = 10
-    django_details = {'name' : cname, 'duration' : duration, 'seats' : seats}
-    return render
-    
