e04bede3ecda4ef8dbcbc0fc5fd5fec800d6ea74
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-10 19:50:52 +0530

    model admin

--- New folder/College/enroll/admin.py
+++ New folder/College/enroll/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Student
 
 # Register your models here.
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student_id','student_name', 'student_email', 'student_password','comment')
+
+admin.site.register(Student, StudentAdmin)
