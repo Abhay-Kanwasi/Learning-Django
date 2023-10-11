5ced54fc5a9b32fb2f5c23264af758dbd826443e
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-11 11:01:12 +0530

    form validation and sucess submission

--- New folder/College/enroll/urls.py
+++ New folder/College/enroll/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path('stu/',views.studentinfo),
     # path('register/',views.showformdata),
-    path('data/',views.showformdata)
+    path('data/',views.showformdata),
+    path('sucess/', views.thankyou)
 ]
--- New folder/College/enroll/views.py
+++ New folder/College/enroll/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from enroll.models import Student
 from enroll.forms import StudentRegistration
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -14,6 +15,10 @@ from enroll.forms import StudentRegistration
 #     fm.order_fields(field_order=['email','name'])
 #     return render(request, 'enroll/register.html', {'form':fm})
 
+
+def thankyou(request):
+    return render(request, 'enroll/sucess.html')
+
 def showformdata(request):
     if request.method == "POST":
         fm = StudentRegistration(request.POST)
@@ -21,6 +26,12 @@ def showformdata(request):
             print("Form Validation")
             print('Name :', fm.cleaned_data['name'])
             print('Email :', fm.cleaned_data['email'])
+
+            # method 1
+            # return render(request, 'enroll/sucess.html',{'form':fm})
+
+            # method 2
+            return HttpResponseRedirect('/enroll/sucess/')
     else:
         fm = StudentRegistration()
     return render(request, 'enroll/register.html', {'form' : fm})
