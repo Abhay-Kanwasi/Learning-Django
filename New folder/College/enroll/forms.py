366f268950bbedf75c98b8077841b98a0a94a982
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-13 10:14:35 +0530

    custom validation form

--- New folder/College/enroll/forms.py
+++ New folder/College/enroll/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 class StudentRegistration(forms.Form):
 
@@ -37,16 +38,70 @@ class StudentRegistration(forms.Form):
 
     #######################################################################################
 
-    data = forms.CharField(min_length=5, max_length=30, strip=False)
+    # data = forms.CharField(min_length=5, max_length=30, strip=False)
 
     # data = forms.CharField(empty_value='Sonam') # in case of empty value error message will not work
 
     # data = forms.CharField(error_messages={'required':'Please enter your name'})
 
 
-    roll_number = forms.IntegerField(min_value=5)
-    agree = forms.BooleanField(label="I agree", label_suffix=" ")
+    # roll_number = forms.IntegerField(min_value=5)
+    # agree = forms.BooleanField(label="I agree", label_suffix=" ")
 
+#############################################################################################
 
+    # name = forms.CharField(max_length=5, required=False)
+    # # roll_number = forms.CharField(widget=forms.IntegerField())
+    # price = forms.DecimalField()
+    # rate = forms.FloatField()
+    # comment = forms.SlugField()
+    # email = forms.EmailField()
+    # website = forms.URLField()
+    # password = forms.CharField(widget=forms.PasswordInput())
+    # description  = forms.CharField(widget=forms.Textarea)
+    # feedback = forms.CharField(min_length=5, max_length=20, widget=forms.TextInput(attrs={'class': 'somecss1 somecss2', 'id' : 'uniqeid'}))
+    # notes = forms.CharField(widget=forms.Textarea(attrs={'rows':3, 'cols' : 50}))
+    # agree = forms.BooleanField(label_suffix='', label="I agree with terms and conditions")
+
+###############################################################################
+
+# validate a specific field
+
+    # name = forms.CharField()
+    # email = forms.EmailField()
+    # password = forms.CharField(widget=forms.PasswordInput)
+    # def clean_name(self):
+    #     valname = self.cleaned_data['name']
+    #     if len(valname)<4:
+    #         raise forms.ValidationError("Enter more than or equal 4")
+    #     return valname
     
+##############################################################################
+    
+# validate of complete form
 
+    # name = forms.CharField()
+    # email = forms.EmailField()
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     valname = self.cleaned_data['name']
+    #     if len(valname) < 4:
+    #         raise forms.ValidationError("Name should be more than or equal 4")
+    #     valemail = self.cleaned_data['email']
+    #     if len(valemail) < 10:
+    #         raise forms.ValidationError("Email should be more than or equal 10")
+
+##################################################################################
+    # built-in validators
+
+    # name = forms.CharField(validators=[validators.MaxLengthValidator(10)])
+    # email = forms.EmailField()
+
+####################################################################################
+
+    def starts_with_s(value):
+        if value[0] != 's':
+            raise forms.ValidationError('Name should starts with s')
+    
+    name = forms.CharField(validators=[starts_with_s])
+    email = forms.EmailField()
--- New folder/College/enroll/views.py
+++ New folder/College/enroll/views.py
@@ -39,14 +39,37 @@ from django.http import HttpResponseRedirect
 
 ###################################################################################
 
+# def showformdata(request):
+#     if request.method == "POST":
+#         fm = StudentRegistration(request.POST)
+#         if fm.is_valid():
+#             print('Form Validation')
+#             print("Name ", fm.cleaned_data['name'])
+#             # print("Roll_number ", fm.cleaned_data['roll_number']) 
+#             print("price ", fm.cleaned_data['price'])
+#             print("Rate ", fm.cleaned_data['rate'])
+#             print("Comment ", fm.cleaned_data['comment'])
+#             print("Email ", fm.cleaned_data['email'])
+#             print("Website ", fm.cleaned_data['website'])
+#             print("Password ", fm.cleaned_data['password'])
+#             print("Description ", fm.cleaned_data['description'])
+#             print("Feedback ", fm.cleaned_data['feedback'])
+#             print("Notes ", fm.cleaned_data['notes'])
+#             print("Agree ", fm.cleaned_data['agree'])
+#         else:
+#             fm = StudentRegistration()
+#     return render(request, 'enroll/register.html', {'form' : fm})
+
+###################################################################################
+
 def showformdata(request):
-   if request.method == "POST":
-        fm = StudentRegistration(request.POST)
+    fm = StudentRegistration(request.POST)
+    if request.method == "POST":
         if fm.is_valid():
-            print('Form Validation')
-            print('Data :', fm.cleaned_data['data'])
-            print('Agree :', fm.cleaned_data['agree'])
-            print('Roll No. :', fm.cleaned_data['roll_number'])
-   else:
-       fm = StudentRegistration()
-   return render(request, 'enroll/register.html', {'form' : fm})
+            print("Name ", fm.cleaned_data['name'])
+            print("Email ", fm.cleaned_data['email'])
+    else:
+        fm = StudentRegistration()
+    return render(request, 'enroll/register.html', {'form':fm})
+
+#######################################################################################
