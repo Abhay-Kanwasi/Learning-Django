263ffd0b6a65bbc4d217e45ace321fb195f41a5f
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-14 17:44:34 +0530

    more than one path in url

--- New folder/dynamicurl/enroll/urls.py
+++ New folder/dynamicurl/enroll/urls.py
@@ -3,5 +3,6 @@ from enroll import views
 
 urlpatterns = [
     path('home/',views.home, name="home"),
-    path('show/<my_id>/',views.show_details, name='detail')
+    path('show/<int:my_id>/',views.show_details, name='detail'),
+    path('show/<int:my_id>/<int:subid>',views.show_subdetails, name='subdetail'),
 ]
--- New folder/dynamicurl/enroll/views.py
+++ New folder/dynamicurl/enroll/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'enroll/home.html')
 
-def show_details(request, my_id):
-    print(my_id)
-    return render(request, 'enroll/show.html',{'id':my_id}) 
+# def show_details(request, my_id):
+#     print(my_id)
+#     return render(request, 'enroll/show.html',{'id':my_id}) 
+
+def show_details(request,my_id):
+    if my_id == 1:
+        student = {'id':my_id, 'name':'Abhay Kanwasi'}
+    if my_id == 2:
+        student = {'id':my_id, 'name':'Abhay Bhardwaj'}
+    if my_id == 3:
+        student = {'id':my_id, 'name':'Aman Bisht'}
+    return render(request, 'enroll/show.html',student)
+
+def show_subdetails(request,my_id, subid):
+    if my_id == 1 and subid==5:
+        student = {'id':my_id, 'name':'Abhay Kanwasi', 'info':subid}
+    if my_id == 2 and subid==9:
+        student = {'id':my_id, 'name':'Abhay Bhardwaj', 'info':subid}
+    if my_id == 3 and subid==3:
+        student = {'id':my_id, 'name':'Aman Bisht', 'info':subid}
+    return render(request, 'enroll/show.html',student)
