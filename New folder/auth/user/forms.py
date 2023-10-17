49cd96d1f97733825017deae85639c6daeba5622
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-17 22:33:01 +0530

    register feature completed

--- New folder/auth/user/forms.py
+++ New folder/auth/user/forms.py
@@ -1,7 +1,11 @@
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SignUpForm(UserCreationForm):
+    password2 = forms.CharField(label="Password(again)", widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields = ['username', 'firstname', 'lastname', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email']
+        labels = {'email':'Email'}
--- New folder/auth/user/views.py
+++ New folder/auth/user/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .forms import SignUpForm
+from django.contrib import messages
 
 # Create your views here.
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
+            messages.success(request, 'You loginned successfully!')
             form.save()
     else:
         form = SignUpForm()
