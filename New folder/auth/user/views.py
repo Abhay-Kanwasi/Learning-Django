01bbcd722e219ef2fed8ba6db20c7a02511de0d5
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-17 21:10:50 +0530

    usercreationform more fields

--- New folder/auth/user/views.py
+++ New folder/auth/user/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'user/home.html', {'form':form}) 
