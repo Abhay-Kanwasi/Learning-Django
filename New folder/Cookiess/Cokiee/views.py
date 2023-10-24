bbb33cd7bfa1d4ab1a825ec5dad3fd4398d4af72
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-24 15:51:44 +0530

    cookies completed

--- New folder/Cookiess/Cokiee/views.py
+++ New folder/Cookiess/Cokiee/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def setcookie(request):
+    response = render(request, 'setcookie.html')
+    response.set_cookie('name', 'abhay')
+    return response
+
+def getcookie(request):
+
+    # method 1
+    # name = request.COOKIES['name']
+
+    # method 2
+    name = request.COOKIES.get('name',"default value")
+
+    return render(request, 'getcookie.html', {'name': name})
+
+def delcookie(request):
+    response = render(request, 'delcookie.html')
+    response.delete_cookie('name')
+    return response
+
+
+def signed_set_cookie(request):
+    response = render(request, 'setsignedcookie.html')
+    response.set_signed_cookie('name', 'abhay', salt="nm", expires = datetime.utcnow()+timedelta(days=2))
+    return response
+
+def signed_get_cookie(request):
+    name = request.get_signed_cookie('name', salt='nm')
+    return render(request, 'getsignedcookie.html', {'name': name})
