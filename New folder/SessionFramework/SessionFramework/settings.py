a43d050f65e09afc9c92639d816bc77a58df4088
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-10-25 16:24:21 +0530

    overide session methods form settings

--- New folder/SessionFramework/SessionFramework/settings.py
+++ New folder/SessionFramework/SessionFramework/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -122,3 +122,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Session and Cookie default settings override
+
+# all of these sessions will add a new session not append in existing session
+SESSION_COOKIE_AGE = 400
+SESSION_COOKIE_NAME = 'sessionname' # by default it is sessionid
+SESSION_COOKIE_PATH = '/home' 
