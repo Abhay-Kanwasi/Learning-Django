886dbcf0f012d53c1c2057ec453c875c23696ce8
Abhay Kanwasi <djoker682@gmail.com>
2023-11-07 15:35:03 +0530

    something

--- New folder/UserAdminProfile/UserAdmin/views.py
+++ New folder/UserAdminProfile/UserAdmin/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-
+from django.shortcuts import redirect, HttpResponseRedirect
+from django.conf import settings
 
 from .forms import SignUpForm, EditUserProfileForm, EditAdminProfileForm
 
-
+def activateEmail(request, user, to_email):
+    subject = 'Thank you for registering to our site'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['receiver@gmail.com',]
+    send_mail(subject, message, email_from, recipient_list)
+    return render(request, 'verification_email.html')
 def sign_up(request):
     if request.method == "POST":
         fm = SignUpForm(request.POST)
         if fm.is_valid():
+            user = fm.save(commit=False)
+            user.is_active = False
+            user.save()
+            activateEmail(request, user, fm.cleaned_data.get('email'))
             messages.success(request, 'Account Created Successfully !')
-            fm.save()
+            return render(request, 'signup.html', {'form':fm})
     else:
         fm = SignUpForm()
     return render(request, 'signup.html', {'form':fm})
--- New folder/UserAdminProfile/UserAdminProfile/settings.py
+++ New folder/UserAdminProfile/UserAdminProfile/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Email Verification Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djoker682@gmail.com'
+EMAIL_HOST_PASSWORD = 'Alex4617P'
 
 # Application definition
 
