2ea56dd85ee2b8b4dbc501014f2a3bea05ee3904
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-10-09 23:50:37 +0530

    models added

--- 30_September/dummyapp/models.py
+++ 30_September/dummyapp/models.py
@@ -1,3 +1,9 @@
+"""
+Model Relationships
+
+Django offers way to define the 3 most common types of database relationships.
+"""
+
 from django.db import models
 
 # Create your models here.
@@ -7,3 +13,13 @@ class Product(models.Model):
     quantity = models.CharField(max_length=25)
     category = models.CharField(max_length=25)
     is_available = models.CharField(max_length=25)
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=25)
+
+# Create table called <app-name>_student
+# Add columns : id, name,
+# Example: student1 = Student(name='Abhay', age=24)
+# student1.save() # Inserts into the database.
